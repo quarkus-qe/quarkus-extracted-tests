8241bfea0a8ac10ddcb277d2c2cfff953c637fa3
QuarkusQE <quarkus-qe@redhat.com>
2025-03-06 19:48:57 +0000

    Add tests extracted from d1455cc4abc

---
+++ integration-tests/jpa-oracle/src/test/java/io/quarkus/it/jpa/oracle/JPAFunctionalityInGraalITCase.java
@@ -0,0 +1,11 @@
+package io.quarkus.it.jpa.oracle;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+/**
+ * Test various JPA operations running in SubstrateVM
+ */
+@QuarkusIntegrationTest
+public class JPAFunctionalityInGraalITCase extends JPAFunctionalityTest {
+
+}
