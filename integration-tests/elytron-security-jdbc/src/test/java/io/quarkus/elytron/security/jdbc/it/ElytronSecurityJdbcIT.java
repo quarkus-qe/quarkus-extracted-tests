59f137b750c27243416705987551ca301f42f8e8
QuarkusQE <quarkus-qe@redhat.com>
2025-03-06 15:36:58 +0000

    Add tests extracted from d1455cc4abc

---
+++ integration-tests/elytron-security-jdbc/src/test/java/io/quarkus/elytron/security/jdbc/it/ElytronSecurityJdbcIT.java
@@ -0,0 +1,8 @@
+package io.quarkus.elytron.security.jdbc.it;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class ElytronSecurityJdbcIT extends ElytronSecurityJdbcTest {
+
+}
