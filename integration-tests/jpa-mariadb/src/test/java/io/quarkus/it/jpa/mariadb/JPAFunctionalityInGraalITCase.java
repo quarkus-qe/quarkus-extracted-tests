31e34fb83b1ab8c05d52202e1d22646750cfd8d2
QuarkusQE <quarkus-qe@redhat.com>
2025-03-13 04:15:41 +0000

    Add tests extracted from db5ec0efe77

---
+++ integration-tests/jpa-mariadb/src/test/java/io/quarkus/it/jpa/mariadb/JPAFunctionalityInGraalITCase.java
@@ -0,0 +1,11 @@
+package io.quarkus.it.jpa.mariadb;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+/**
+ * Test various JPA operations running in native mode
+ */
+@QuarkusIntegrationTest
+public class JPAFunctionalityInGraalITCase extends JPAFunctionalityTest {
+
+}
