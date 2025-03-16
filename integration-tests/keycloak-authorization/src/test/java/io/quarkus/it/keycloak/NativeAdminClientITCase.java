9ac41f9f3a8e480bb264c38f0902bee1ad338264
QuarkusQE <quarkus-qe@redhat.com>
2025-03-16 20:55:14 +0000

    Add tests extracted from 15dc24a55fe

---
+++ integration-tests/keycloak-authorization/src/test/java/io/quarkus/it/keycloak/NativeAdminClientITCase.java
@@ -0,0 +1,9 @@
+package io.quarkus.it.keycloak;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class NativeAdminClientITCase extends AdminClientTestCase {
+
+    // Execute the same tests but in native mode.
+}
