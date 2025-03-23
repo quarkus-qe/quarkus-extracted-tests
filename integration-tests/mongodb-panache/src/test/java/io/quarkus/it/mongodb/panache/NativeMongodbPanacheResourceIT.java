6e695636ddd2e346212caef67146f26f0566b108
QuarkusQE <quarkus-qe@redhat.com>
2025-03-23 19:38:28 +0000

    Add tests extracted from b9777af7cd4

---
+++ integration-tests/mongodb-panache/src/test/java/io/quarkus/it/mongodb/panache/NativeMongodbPanacheResourceIT.java
@@ -0,0 +1,8 @@
+package io.quarkus.it.mongodb.panache;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class NativeMongodbPanacheResourceIT extends MongodbPanacheResourceTest {
+
+}
