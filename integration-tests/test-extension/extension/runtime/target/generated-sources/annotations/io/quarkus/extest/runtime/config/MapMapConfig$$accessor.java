231e84d13c34587f79268cc1773f3e8d65973551
QuarkusQE <quarkus-qe@redhat.com>
2025-03-23 21:30:55 +0000

    Add tests extracted from b9777af7cd4

---
+++ integration-tests/test-extension/extension/runtime/target/generated-sources/annotations/io/quarkus/extest/runtime/config/MapMapConfig$$accessor.java
@@ -0,0 +1,13 @@
+package io.quarkus.extest.runtime.config;
+@io.quarkus.Generated("Quarkus annotation processor")
+public final class MapMapConfig$$accessor {
+    private MapMapConfig$$accessor() {}
+    @SuppressWarnings("unchecked")
+    public static Object get_map(Object __instance) {
+        return ((MapMapConfig)__instance).map;
+    }
+    @SuppressWarnings("unchecked")
+    public static void set_map(Object __instance, Object map) {
+        ((MapMapConfig)__instance).map = (java.util.Map<String, java.util.Map<String, String>>)map;
+    }
+}
