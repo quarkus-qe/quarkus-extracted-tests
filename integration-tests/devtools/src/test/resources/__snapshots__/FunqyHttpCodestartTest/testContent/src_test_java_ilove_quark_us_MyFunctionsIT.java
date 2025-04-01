864528e795e61aa606fbfd01a56367266c0bfb3a
QuarkusQE <quarkus-qe@redhat.com>
2025-04-01 11:22:08 +0000

    Add tests extracted from 3525bf49fc0

---
+++ integration-tests/devtools/src/test/resources/__snapshots__/FunqyHttpCodestartTest/testContent/src_test_java_ilove_quark_us_MyFunctionsIT.java
@@ -0,0 +1,8 @@
+package ilove.quark.us;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class MyFunctionsIT extends MyFunctionsTest {
+    // Run the same tests in native
+}
