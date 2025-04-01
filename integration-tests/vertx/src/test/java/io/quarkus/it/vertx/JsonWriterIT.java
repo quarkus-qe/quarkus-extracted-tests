cd677a9bb22e31f75ee0358dca4a41a6db111b7c
QuarkusQE <quarkus-qe@redhat.com>
2025-04-01 14:23:05 +0000

    Add tests extracted from 15d328f8fab

---
+++ integration-tests/vertx/src/test/java/io/quarkus/it/vertx/JsonWriterIT.java
@@ -0,0 +1,11 @@
+package io.quarkus.it.vertx;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+/**
+ * @author Thomas Segismont
+ */
+@QuarkusIntegrationTest
+public class JsonWriterIT extends JsonWriterTest {
+
+}
