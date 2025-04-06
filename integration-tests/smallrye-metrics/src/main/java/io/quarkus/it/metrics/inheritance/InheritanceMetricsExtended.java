cca68b5eb29e7ad1087b9296d39825bf259dcf24
QuarkusQE <quarkus-qe@redhat.com>
2025-04-06 22:57:31 +0000

    Add tests extracted from 3525bf49fc0

---
+++ integration-tests/smallrye-metrics/src/main/java/io/quarkus/it/metrics/inheritance/InheritanceMetricsExtended.java
@@ -0,0 +1,12 @@
+package io.quarkus.it.metrics.inheritance;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class InheritanceMetricsExtended extends InheritanceMetricsBase {
+
+    public void anotherMethod() {
+
+    }
+
+}
