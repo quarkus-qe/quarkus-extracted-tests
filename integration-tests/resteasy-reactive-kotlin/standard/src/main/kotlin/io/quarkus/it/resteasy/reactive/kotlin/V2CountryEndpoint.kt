8561b044959db0a6f4c0e81403f7ddd54dfd703e
QuarkusQE <quarkus-qe@redhat.com>
2025-05-13 12:00:04 +0000

    Add tests extracted from 3525bf49fc0

---
+++ integration-tests/resteasy-reactive-kotlin/standard/src/main/kotlin/io/quarkus/it/resteasy/reactive/kotlin/V2CountryEndpoint.kt
@@ -0,0 +1,10 @@
+package io.quarkus.it.resteasy.reactive.kotlin
+
+import jakarta.ws.rs.GET
+import jakarta.ws.rs.Path
+
+@Path("v2")
+class V2CountryEndpoint {
+
+    @GET @Path("/name/{name}") fun byName(name: String) = listOf(Country(name, "$name-capital"))
+}
