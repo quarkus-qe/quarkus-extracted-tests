2e889330006f3122e54f520fee1d7a598a863927
QuarkusQE <quarkus-qe@redhat.com>
2025-06-26 03:15:14 +0000

    Add tests extracted from ec69da77faf

---
+++ integration-tests/jpa-without-entity/src/test/java/org/test/NativeHelloResourceIT.java
@@ -0,0 +1,9 @@
+package org.test;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class NativeHelloResourceIT extends HelloResourceTest {
+
+    // Execute the same tests but in native mode.
+}
