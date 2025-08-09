282c1306612b02219e3c74eff462b18ed2ced298
QuarkusQE <quarkus-qe@redhat.com>
2025-08-09 03:19:25 +0000

    Add tests extracted from aa8fa900286

---
+++ integration-tests/hibernate-orm-panache-kotlin/src/test/kotlin/io/quarkus/it/panache/TestResources.kt
@@ -0,0 +1,6 @@
+package io.quarkus.it.panache
+
+import io.quarkus.test.common.QuarkusTestResource
+import io.quarkus.test.h2.H2DatabaseTestResource
+
+@QuarkusTestResource(H2DatabaseTestResource::class) class TestResources
