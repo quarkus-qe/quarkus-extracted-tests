199cc51a33a3a7a748391eb54b112c10bea6fa7e
QuarkusQE <quarkus-qe@redhat.com>
2025-09-25 02:46:45 +0000

    Add tests extracted from 776465775f7

---
+++ integration-tests/elytron-resteasy/src/test/java/io/quarkus/it/resteasy/elytron/BaseAuthIT.java
@@ -0,0 +1,7 @@
+package io.quarkus.it.resteasy.elytron;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class BaseAuthIT extends BaseAuthTest {
+}
---
+++ integration-tests/smallrye-graphql-client/src/test/java/io/quarkus/it/smallrye/graphql/client/TypesafeClientIT.java
@@ -0,0 +1,7 @@
+package io.quarkus.it.smallrye.graphql.client;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class TypesafeClientIT extends TypesafeClientTest {
+}
