eb7f88599938b918603d89628b4c9238b43f95cd
QuarkusQE <quarkus-qe@redhat.com>
2025-10-06 09:58:04 +0000

    Add tests extracted from 14c0ea43569

---
+++ integration-tests/jpa/src/test/java/io/quarkus/it/jpa/attributeconverter/AttributeConverterInGraalITCase.java
@@ -0,0 +1,7 @@
+package io.quarkus.it.jpa.attributeconverter;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class AttributeConverterInGraalITCase extends AttributeConverterTest {
+}
