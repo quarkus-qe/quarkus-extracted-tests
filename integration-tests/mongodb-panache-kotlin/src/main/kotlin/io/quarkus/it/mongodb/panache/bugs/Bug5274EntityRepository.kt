b8f167d9fff01fbc1cc5ea387d6cefe9cbe7edd5
QuarkusQE <quarkus-qe@redhat.com>
2026-01-28 19:53:13 +0000

    Add tests extracted from 05619b8f8df

---
+++ integration-tests/mongodb-panache-kotlin/src/main/kotlin/io/quarkus/it/mongodb/panache/bugs/Bug5274EntityRepository.kt
@@ -0,0 +1,6 @@
+package io.quarkus.it.mongodb.panache.bugs
+
+import io.quarkus.it.mongodb.panache.book.Book
+import jakarta.enterprise.context.ApplicationScoped
+
+@ApplicationScoped class Bug5274EntityRepository : AbstractRepository<Book>()
