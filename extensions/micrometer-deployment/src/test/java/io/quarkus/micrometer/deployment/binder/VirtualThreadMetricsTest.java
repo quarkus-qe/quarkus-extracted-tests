0aee4a7fc23a62cc70a03e00a13a348a337b5746
QuarkusQE <quarkus-qe@redhat.com>
2026-03-26 04:16:58 +0000

    Add tests extracted from 11389d89ee5

---
+++ extensions/micrometer-deployment/src/test/java/io/quarkus/micrometer/deployment/binder/VirtualThreadMetricsTest.java
@@ -0,0 +1,46 @@
+package io.quarkus.micrometer.deployment.binder;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import jakarta.enterprise.inject.Instance;
+import jakarta.inject.Inject;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledForJreRange;
+import org.junit.jupiter.api.condition.JRE;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.quarkus.micrometer.runtime.binder.virtualthreads.VirtualThreadCollector;
+import io.quarkus.test.QuarkusUnitTest;
+import io.quarkus.test.junit.common.DisabledOnSemeru;
+
+@EnabledForJreRange(min = JRE.JAVA_21)
+@DisabledOnSemeru(reason = "JFR is not supported on Semeru yet")
+public class VirtualThreadMetricsTest {
+
+    @RegisterExtension
+    static final QuarkusUnitTest config = new QuarkusUnitTest()
+            .withConfigurationResource("test-logging.properties")
+            .overrideConfigKey("quarkus.redis.devservices.enabled", "false")
+            .withEmptyApplication();
+
+    @Inject
+    Instance<VirtualThreadCollector> collector;
+
+    @Test
+    void testInstancePresent() {
+        assertTrue(collector.isResolvable(), "VirtualThreadCollector expected");
+    }
+
+    @Test
+    void testBinderCreated() {
+        assertThat(collector.get().getBinder()).isNotNull();
+    }
+
+    @Test
+    void testTags() {
+        assertThat(collector.get().getTags()).isEmpty();
+    }
+
+}
